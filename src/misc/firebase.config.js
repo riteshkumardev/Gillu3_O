d8dde590f9dcb3bef710b2e0488f8e7de18e18f8
riteshkumardev <sriteshkumar11022@gmail.com>
2023-05-28 01:11:32 -0700

    firebase change

--- src/misc/firebase.config.js
+++ src/misc/firebase.config.js
@@ -14,22 +14,18 @@ import { getStorage } from "firebase/storage";
 //   databaseURL:
 //     "https://chat-app-15798-default-rtdb.asia-southeast1.firebasedatabase.app/",
 // }
-
+    
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCivmHHTVT7FpFT0xAJpI89Rvji64l5s1c",
+  apiKey: "AIzaSyCivmHHTVT7FpFT0xAJpI89Rvji64l5s1c", 
+   
   authDomain: "gillu3-o.firebaseapp.com",
   projectId: "gillu3-o",
   storageBucket: "gillu3-o.appspot.com",
   messagingSenderId: "527830563448",
   appId: "1:527830563448:web:ad0a080c1393ea6a0acb8d",
   measurementId: "G-EZNGW8QX6Y",
-    // apiKey: "AIzaSyD2dL96se3yIXIPKOtvtTuPU4vPcmYknf8",
-    // authDomain: "chat-app-15798.firebaseapp.com",
-    // projectId: "chat-app-15798",
-    // storageBucket: "chat-app-15798.appspot.com",
-    // messagingSenderId: "159523893460",
-    // appId: "1:159523893460:web:068a680501040583488b41",
+
     databaseURL:
       "https://gillu3-o-default-rtdb.firebaseio.com",
 
